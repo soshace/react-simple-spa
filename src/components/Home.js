69b0239348d376ed9a97dcfcc2add6c48ed0323e
insanegrv <insanegrv@gmail.com>
2016-07-09 18:06:21 +0300

    Create Booking, BookingList and Home components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Hello! Here will be a home page...
+      </div>
+    )
+  }
+}
+
+export default Home
