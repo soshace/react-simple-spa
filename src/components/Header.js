5494b1b8c9881cae0c28032984e8db7b9eb6cbcf
insanegrv <insanegrv@gmail.com>
2016-07-12 15:18:54 +0300

    Mobile styles for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,49 +12,47 @@ class Header extends Component {
     const { navIsOpen, toggleNav } = this.props
     const navIcon = navIsOpen ? <CloseIcon /> : <BurgerIcon />
 
-    const inputStyle = {
-      display: "inlineBlock",
-      marginBottom: 10,
-      padding: 10,
-      paddingLeft: 50,
-      verticalAlign: "middle",
-      border: "none",
-      borderRadius: 5,
-      backgroundColor: "#448aff",
-      color: "white"
-        // fontFamily: "monospace",
-        // fontSize: "32",
-    }
-
     const headerStyle = navIsOpen ? {
       marginLeft: "20%",
       width: "80%",
-      backgroundColor: '#2979ff'
-    } : null
+      backgroundColor: "#2979ff"
+    } : {backgroundColor: "#2979ff"}
+
+    const titleStyle = {
+      display: "none"
+    }
 
     return (
-        <AppBar
-          title="Messages??"
-          iconElementLeft={<IconButton onTouchTap={toggleNav}>{navIcon}</IconButton>}
-          iconElementRight={
-            <div>
-              <input style={inputStyle} type="text" value="Search"/>
-              <IconButton><NotificationIcon color="white" /></IconButton>
-              <IconMenu
-                iconButtonElement={
-                <IconButton><MoreVertIcon color="white" /></IconButton>
-              }
-              targetOrigin={{horizontal: 'right', vertical: 'top'}}
-              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-              >
-                <MenuItem primaryText="Refresh" />
-                <MenuItem primaryText="Help" />
-                <MenuItem primaryText="Sign out" />
-              </IconMenu>
-            </div>
+      <AppBar className="header" title={null} titleStyle={titleStyle} iconElementLeft={<IconButton onTouchTap={toggleNav}>{navIcon}</IconButton>} style={headerStyle}>
+        <div className="header__title">
+          <h1>Title?</h1>
+        </div>
+        <div className="header__search">
+          <button className="header__search-btn">
+            <svg className="header__search-icon" fill="#fff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+              <path d="M0 0h24v24H0z" fill="none"/>
+            </svg>
+          </button>
+          <input className type="text"/>
+        </div>
+        <div className="header__notification">
+          <IconButton><NotificationIcon color="white" /></IconButton>
+        </div>
+        <div className="header__more">
+          <IconMenu
+            iconButtonElement={
+            <IconButton><MoreVertIcon color="white" /></IconButton>
           }
-          style={headerStyle}
-        />
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          >
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        </div>
+      </AppBar>
     )
   }
 }
