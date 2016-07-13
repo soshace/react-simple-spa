35e60696444a27e3cb436f87131a84c7e79d17c5
insanegrv <insanegrv@gmail.com>
2016-07-13 19:43:18 +0300

    API call for booking list and markup of it's page

--- src/components/BookingList.js
+++ src/components/BookingList.js
@@ -1,16 +1,104 @@
 import React, { Component }  from 'react'
 import ReactRouter, { Link } from 'react-router'
+import onWindowResize from '../decorators/onWindowResize'
+
+import { IconButton, Avatar } from 'material-ui'
+import AvailableIcon from 'material-ui/svg-icons/notification/event-available'
+import UnavailableIcon from 'material-ui/svg-icons/notification/event-busy'
+import ArchiveIcon from 'material-ui/svg-icons/content/archive'
+import StarIcon from 'material-ui/svg-icons/toggle/star'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 
 class BookingList extends Component {
+  state = {
+    list: [],
+    fetching: false,
+    error: '',
+    fetched: false
+  }
+
+  componentDidMount() {
+    this.setState({
+      fetching: true,
+      error: null,
+      fetched: false
+    })
+
+    const http = new XMLHttpRequest()
+    const url = 'http://test.easybook.ie/api/get_list'
+    const params = 'band_id=6&type=enquiry&mode=available&token' + this.props.token
+
+    http.open('POST', url, true);
+
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = () => {
+    	if (http.readyState == 4 && http.status == 200) {
+    		const res = JSON.parse(http.responseText)
+        this.setState({
+          fetching: false,
+          error: res.error,
+          fetched: res.success,
+          list: res.response.slice(1, 21) // take only 20 for now
+        })
+    	}
+    }
+    http.send(params);
+  }
+
   render() {
+    const { isMobile } = this.props
+    const bookings = this.state.list
+    const moreBtn = isMobile ? null : <IconButton><MoreVertIcon color="grey" /></IconButton>
+
+    const listItems = bookings.map((booking) => {
+      const curDate = new Date(booking.enquiry_date)
+      const time = curDate.getHours() + ':' + curDate.getMinutes()
+      const date = curDate.toUTCString().slice(0, 16)
+
+      return (
+        <li key={booking.id} className="booking-list__item">
+          <div className="booking-list__img">
+            <Link to={'/bookings/' + booking.id}>
+              <Avatar src="https://placehold.it/50x50" size={50}/>
+            </Link>
+          </div>
+          <div className="booking-list__main">
+            <Link to={'/bookings/' + booking.id}>
+              <p>{booking.first_name + ' ' + booking.last_name}</p>
+              <p>{date}</p>
+              <p>{booking.venue}</p>
+            </Link>
+          </div>
+          <div className="booking-list__more">
+            <div className="booking-list__time">
+              {time}
+            </div>
+            <div className="booking-list__star">
+              Star?
+            </div>
+            <div className="booking-list__more">
+              {moreBtn}
+            </div>
+          </div>
+        </li>
+      )
+    })
+
     return (
-      <ul>
-        <li><Link to="booking">1 booking</Link></li>
-        <li>2 booking</li>
-        <li>3 booking</li>
-      </ul>
+      <div className="booking-list">
+        <ul className="booking-list__tabs">
+          <li>Available</li>
+          <li>Unavailable</li>
+          <li>Archive</li>
+          <li>Starred</li>
+        </ul>
+        <ul className="booking-list__list">
+          {listItems}
+        </ul>
+      </div>
     )
   }
 }
-
-export default BookingList
+//<img src="https://placehold.it/50x50" />
+export default onWindowResize(BookingList)
