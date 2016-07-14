5907a1c919a810695de2f0c24531b436a10540a9
insanegrv <insanegrv@gmail.com>
2016-07-14 18:29:38 +0300

    Markup and style for booking page (mobile)

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,7 +1,27 @@
 import React, { Component }  from 'react'
+import ReactRouter, { Link } from 'react-router'
 import onWindowResize from '../decorators/onWindowResize'
 
-import { IconButton, Avatar, FloatingActionButton, MenuItem, FlatButton } from 'material-ui'
+import { IconButton, Avatar, List, ListItem } from 'material-ui'
+
+import BackIcon from 'material-ui/svg-icons/hardware/keyboard-backspace'
+import CloseIcon from 'material-ui/svg-icons/navigation/close'
+
+import PhoneIcon from 'material-ui/svg-icons/communication/phone'
+import NotificationIcon from 'material-ui/svg-icons/social/notifications'
+import MessagesIcon from 'material-ui/svg-icons/communication/email'
+
+import CalendarIcon from 'material-ui/svg-icons/action/event'
+import PlaceIcon from 'material-ui/svg-icons/maps/place'
+import EventIcon from 'material-ui/svg-icons/action/assignment'
+import WorkIcon from 'material-ui/svg-icons/action/work'
+import ContactIcon from 'material-ui/svg-icons/action/perm-contact-calendar'
+import PayIcon from 'material-ui/svg-icons/action/payment'
+import DaysLeftIcon from 'material-ui/svg-icons/action/book'
+import NoteIcon from 'material-ui/svg-icons/notification/event-note'
+import TeamIcon from 'material-ui/svg-icons/social/group'
+import TimeIcon from 'material-ui/svg-icons/device/access-time'
+import RefIcon from 'material-ui/svg-icons/action/bookmark'
 
 class Booking extends Component {
   state = {
@@ -28,7 +48,7 @@ class Booking extends Component {
     http.onreadystatechange = () => {
     	if (http.readyState == 4 && http.status == 200) {
     		const res = JSON.parse(http.responseText)
-        console.log(res)
+        console.log(res.response)
 
         this.setState({
           fetching: false,
@@ -41,34 +61,63 @@ class Booking extends Component {
     http.send(params)
   }
 
+  showMessages = () => {
+
+  }
+
   render() {
     const data  = this.state.data
     const name = data.contacts[0].first_name + ' ' + data.contacts[0].last_name
+    const role = data.contacts[0].role
+    const date = data.event_date
+    const place = data.venue
+    const eventType = data.event_type
+    // ???
+    const paid = 'Paid ' + data.deposit + ' of ' + data.total
+    const daysLeft = Math.round((new Date(date) - new Date()) / 1000 / 60 / 60 / 24) + ' Days Left'
 
     return (
       <div>
-        <div className="booking__summary">
-          <div className="booking__summary-top">
-            <div className="">
-              <div className="">
+        <div className="booking">
+          <div className="booking__top">
+            <div className="booking__nav">
+              <IconButton onTouchTap={this.showMessages}><BackIcon color="white" /></IconButton>
+              <IconButton><CloseIcon color="white" /></IconButton>
+            </div>
+            <div className="booking__user">
+              <div className="booking__user-ava">
                 <Avatar src="https://placehold.it/50x50" size={50}/>
               </div>
-              <div className="">
+              <div className="booking__user-name">
                 <p>{name}</p>
-                <small>Admin</small>
+                <small>{role}</small>
               </div>
             </div>
-            <div className="">
-              <FlatButton  label="My Account" labelStyle={ {color: "white"} } hoverColor="none" rippleColor="rgba(0, 0, 0, 0.5)" />
-              <IconButton ><SettingsIcon color="white" /></IconButton>
+            <div className="booking__actions">
+              <IconButton><PhoneIcon color="white" /></IconButton>
+              <IconButton><MessagesIcon color="white" /></IconButton>
+              <IconButton><NotificationIcon color="white" /></IconButton>
             </div>
           </div>
-          <div className="booking__summary-info">
-
+          <div className="booking__info">
+            <List>
+              <ListItem primaryText={date} leftIcon={<CalendarIcon />} />
+              <ListItem primaryText={place} leftIcon={<PlaceIcon />} />
+              <ListItem primaryText={eventType} leftIcon={<EventIcon />} />
+              <ListItem primaryText="???" leftIcon={<WorkIcon />} />
+              <ListItem primaryText="???" leftIcon={<ContactIcon />} />
+              <ListItem primaryText={paid} leftIcon={<PayIcon />} />
+              <ListItem primaryText={daysLeft} leftIcon={<DaysLeftIcon />} />
+              <ListItem primaryText="Notes???" leftIcon={<NoteIcon />} />
+              <ListItem primaryText="Team???" leftIcon={<TeamIcon />} />
+              <ListItem primaryText="Timeline???" leftIcon={<TimeIcon />} />
+              <ListItem primaryText="Ref???" leftIcon={<RefIcon />} />
+            </List>
           </div>
         </div>
-        <div className="booking__messages">
 
+        <div className="messages">
+          CHAT
         </div>
       </div>
     )
